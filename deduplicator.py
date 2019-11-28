74f51b45353aac125fc5d1fa5a24ed514bd70ed2
Pseudozz <kchamilton2011@gmail.com>
2019-11-27 21:56:20 -0600

    updated variable name and changed order of code

--- deduplicator.py
+++ deduplicator.py
@@ -2,12 +2,13 @@ import os
 import shutil
 import hashlib
 
-hash_list = {}
-user_input = raw_input("Enter path to images: ")
-copy_dir = raw_input("Enter path to copy images to: ")
-assert os.path.exists(user_input), "Couldn't find directory "+str(user_input)
+hash_dict = {}
 
+user_input = raw_input("Enter path to images: ")
+assert os.path.exists(user_input), "Couldn't find directory "+str(user_input)
 os.chdir(user_input)
+
+copy_dir = raw_input("Enter path to copy images to: ")
 if not os.path.exists(copy_dir):
     os.mkdir(copy_dir)
 
@@ -17,7 +18,7 @@ for dirName, subdirList, fileList in os.walk(user_input):
         image_contents = imagefile.read()
         fileHash = hashlib.md5(image_contents).hexdigest()
         imagefile.close()
-        if fileHash not in hash_list.keys():
-            hash_list[fileHash] = fname
+        if fileHash not in hash_dict.keys():
+            hash_dict[fileHash] = fname
             print(fileHash + " : " + fname)
             shutil.copy(fname, copy_dir)
